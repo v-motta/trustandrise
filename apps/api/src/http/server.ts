ac02bd064dbaa4a350f1f38371bb3e8fbc1fb51f
v-motta <vinicius.costa@trescon.com.br>
2024-08-18 23:11:01 -0300

    initial commit

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,49 @@
+import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
+import fastifySwagger from "@fastify/swagger";
+
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { env } from "@repo/env";
+import fastify from "fastify";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+// app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next SaaS RBAC',
+      description: 'Full-Stack SaaS app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(fastifyCors)
+
+app.listen({ port: env.SERVER_PORT }).then(() => {
+  console.log(`Server listening on http://localhost:${env.SERVER_PORT}`)
+})
